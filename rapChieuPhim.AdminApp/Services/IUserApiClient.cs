9d97ab101f48eb916b50173bbca6f1997056a95e
Folio_9470m <Folio_9470m@DESKTOP-ADL5M0V>
2020-12-26 10:50:32 +0700

    login bosttrap done

---
+++ rapChieuPhim.AdminApp/Services/IUserApiClient.cs
@@ -0,0 +1,13 @@
+﻿using rapChieuPhim.ViewModels.System.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rapChieuPhim.AdminApp.Services
+{
+    public interface IUserApiClient
+    {
+        Task<string> Authenticate(LoginRequest request);
+    }
+}
